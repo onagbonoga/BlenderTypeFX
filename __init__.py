92419773915db2470979a669d8bc4d227d230309
Nurudeen Agbonoga <nurudeenagbonoga@gmail.com>
2023-06-14 10:57:36 -0500

    initial working ver before cleanup and optimize

---
+++ __init__.py
@@ -0,0 +1,190 @@
+'''
+Copyright (C) 2023 Nurudeen Agbonoga
+
+Created by Nurudeen Agbonoga
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+bl_info = {
+    "name": "BlenderTypeFX",
+    "blender": (3,5,0),
+    "category": "Animation",
+    "version": (1,0),
+    "description": "Create animated text effects in blender with ease",
+    "location": "3D Viewport > Properties tab"
+}
+
+import bpy
+
+# animate text operator
+class TA_Animate_Text(bpy.types.Operator):
+    '''animate text'''
+    bl_idname = "ta.animate"
+    bl_label = "text animate animate"
+    bl_options = {'REGISTER'}
+    
+    def execute(self, context): # runs when called
+        # spawn text
+        text = bpy.context.scene.ta_text
+        font = bpy.context.scene.ta_font
+        frameStep = bpy.context.scene.ta_frame_step
+        startFrame = bpy.context.scene.ta_start
+        wordsPerLine = bpy.context.scene.ta_n_per_line
+        textFile = bpy.context.scene.ta_text_file
+        
+        # load text from text file if available
+        if not(textFile == "" or textFile == " "): 
+            textFilePath = bpy.path.abspath(textFile)
+            with open(textFilePath) as f:
+                text = f.read()
+                print(text)
+        # add text one by one at specified radius with spacing
+        locationX = 0
+        locationY = 0
+        radius = 0.3
+        showFrame = startFrame
+        # add empty to control position of everything
+        bpy.ops.object.empty_add(type="PLAIN_AXES")
+        emptyName = bpy.context.scene.objects[-1].name
+        wordCount = 0
+        for c in text:
+            bpy.ops.object.text_add(radius = radius, location = [locationX,locationY,0])
+            bpy.context.object.data.body = c
+            
+            # use custom font if specified
+            if not(font == "" or font == " "):
+                fnt = bpy.data.fonts.load(font)
+                bpy.context.object.data.font = fnt
+            
+            # set key frames
+            
+            # parent object to emptty
+            objectName = bpy.context.scene.objects[-1].name
+            bpy.data.objects[objectName].parent = bpy.data.objects[emptyName]
+            
+            # hide everything in the first frame
+            bpy.data.objects[objectName].hide_render = True
+            bpy.data.objects[objectName].hide_viewport = True
+            bpy.data.objects[objectName].keyframe_insert(data_path = "hide_viewport", frame = 0)
+            bpy.data.objects[objectName].keyframe_insert(data_path = "hide_render", frame = 0)
+            
+            # show object in the specified frame
+            bpy.data.objects[objectName].hide_render = False
+            bpy.data.objects[objectName].hide_viewport = False
+            bpy.data.objects[objectName].keyframe_insert(data_path = "hide_viewport", frame = showFrame)
+            bpy.data.objects[objectName].keyframe_insert(data_path = "hide_render", frame = showFrame)
+            
+            locationX += (radius/2)
+            showFrame += frameStep
+            
+            # keep track of word count and update y location
+            if c == "\r" or c == "\n" or c == "\r\n":
+                locationY -= (radius)
+                locationX = 0
+                wordCount = 0
+            elif c == " ":
+                wordCount += 1
+                if wordCount % wordsPerLine == 0:
+                    locationY -= (radius)
+                    locationX = 0
+        
+            
+        return {'FINISHED'}
+    
+# reset operator
+class TA_Animate_Reset(bpy.types.Operator):
+    '''reset text'''
+    bl_idname = "ta.reset"
+    bl_label = "text animate reset"
+    bl_options = {'REGISTER'}
+    
+    def execute(self, context): # runs when called
+        # ainimate text
+        return {'FINISHED'}
+    
+class TA_PT_View(bpy.types.Panel):
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "BLENDER_TYPEFX"
+    bl_label = "Animate Text input"
+    
+    def draw(self, context):
+        # text input field
+        row = self.layout.row()
+        row.label(text="Enter Text Below:")
+        row = self.layout.row()
+        row.prop(context.scene, "ta_text")
+        
+        # text file input field
+        row = self.layout.row()
+        row.label(text="Text file:")
+        row = self.layout.row()
+        row.label(text="this will overwrite the text box",icon = "INFO")
+        row = self.layout.row()
+        row.prop(context.scene, "ta_text_file")
+        
+        # font input field
+        row = self.layout.row()
+        row.label(text="Font:")
+        row = self.layout.row()
+        row.prop(context.scene, "ta_font")
+        
+        # frame start field
+        row = self.layout.row()
+        row.label(text="Start Frame:")
+        row = self.layout.row()
+        row.prop(context.scene, "ta_start")
+        
+        # number of words per line field
+        row = self.layout.row()
+        row.label(text="Number of Words Per Line:")
+        row = self.layout.row()
+        row.prop(context.scene, "ta_n_per_line")
+        
+        # number of frames between characters field
+        row = self.layout.row()
+        row.label(text="Frame Step:")
+        row = self.layout.row()
+        row.prop(context.scene, "ta_frame_step")
+        
+        # animate button
+        self.layout.operator("ta.animate", text = "Animate",
+        icon = "PLAY")
+        
+        # reset button
+        #self.layout.operator("ta.reset", text = "Reset")
+        
+def register():
+    
+    # register propertiies
+    bpy.types.Scene.ta_text = bpy.props.StringProperty(name="")
+    bpy.types.Scene.ta_text_file = bpy.props.StringProperty(name="", subtype='FILE_PATH')
+    bpy.types.Scene.ta_start = bpy.props.IntProperty(name="", default = 1)
+    bpy.types.Scene.ta_n_per_line = bpy.props.IntProperty(name="", default = 10)
+    bpy.types.Scene.ta_frame_step = bpy.props.IntProperty(name="", default = 1)
+    bpy.types.Scene.ta_font = bpy.props.StringProperty(name="", subtype='FILE_PATH')
+    
+    # register panel
+    bpy.utils.register_class(TA_PT_View)
+    
+    # register operators
+    bpy.utils.register_class(TA_Animate_Text)
+    #bpy.utils.register_class(TA_Animate_Reset)
+    
+def unregister():
+    bpy.utils.unregister_class(TA_PT_View)
+    bpy.utils.unregister_class(TA_Animate_Text)
+    #bpy.utils.unregister_class(TA_Animate_Reset)
+    
+register()
